91b33fc297f61c02b0c052ce5dc885ae186d7aec
Utkarsh <43597586+utkarshs106@users.noreply.github.com>
2024-09-08 22:07:24 +0530

    DTO modified

---
+++ src/main/java/com/example/productservice/DTO/AddProductDTO.java
@@ -0,0 +1,11 @@
+package com.example.productservice.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AddProductDTO {
+    String ProducType;
+    Object ProductData;
+}
---
+++ src/main/java/com/example/productservice/DTO/FindProductDto.java
@@ -0,0 +1,11 @@
+package com.example.productservice.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FindProductDto {
+    int id;
+    String productCategory;
+}
